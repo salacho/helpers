b9500988c21e9319994bb48d3bb412db16ac16f0
Andres Salazar <salacho@bu.edu>
2016-01-23 21:40:54 -0500

    Fixed bug in zScoreData, was using Var. to scale the data, not St. Dev. Now it is working.

--- zScoreData.m
+++ zScoreData.m
@@ -12,6 +12,8 @@ function zScoredData = zScoreData(dataOrig)
 %                   z-scored data after processing on each trial.
 %
 % Andres    : v1    : init. 12 Sept 2015 
+% Andres    : v2    : Fixed bug that was dividing by variance, not standard-deviation, 
+%                     as it should have been done since the beginning. 23 Jan 2016
 
 if ndims(dataOrig) == 2     %#ok<*ISMAT>
 elseif ndims(dataOrig) == 3
@@ -21,8 +23,10 @@ elseif ndims(dataOrig) == 3
     for iTrial = 1:size(dataOrig,2)
         trialData = squeeze(dataOrig(:,iTrial,:));
         trialMean = repmat(mean(trialData,2),[1 size(dataOrig,3)]);         % trial mean
-        trialVar = repmat(var(trialData')',[1 size(dataOrig,3)]);           %#ok<UDIM> % need to do this to avoid 'var' thinking we are inputting weights to the variance calculation
-        zScoredData(:,iTrial,:) =  (trialData - trialMean)./trialVar;
+        %trialVar = repmat(var(trialData')',[1 size(dataOrig,3)]);          %#ok<UDIM> % need to do this to avoid 'var' thinking we are inputting weights to the variance calculation
+        %zScoredData(:,iTrial,:) =  (trialData - trialMean)./trialVar;
+        trialStDev = repmat(std(trialData,0,2),[1 size(dataOrig,3)]);
+        zScoredData(:,iTrial,:) =  (trialData - trialMean)./trialStDev;
     end
 else
     error('The number of dimmensions of the input data dataOrig (%i) is incorrect!!!',ndims(dataOrig))
